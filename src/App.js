357afcfc5f0e33156177a9d7e818d7bb7296e934
Monika Fac <monika.fac@gmail.com>
2019-09-27 14:16:18 +0200

    add addItem method

--- src/App.js
+++ src/App.js
@@ -5,50 +5,145 @@ import Item from "./components/Item/Item"
 class App extends React.Component {
   state={
     item:[{
-    id:1,
+    id:0,
     image:"",
     name:"jabłka",
-    amount:"30",
+    avaliableAmount:30,
+    amount:0,
     cost:2,
     
   },
   {
-    id:2,
+    id:1,
     image:"",
     name:"mleko",
-    amount:"10",
+    avaliableAmount:10,
+    amount:0,
     cost:3,
   },
   {
-    id:3,
+    id:2,
     image:"",
     name:"marchewka",
-    amount:"50",
+    avaliableAmount:50,
+    amount:0,
     cost:0.8,
   }
-]
+],
+
+sum:0,
+allAmount:0,
+avaliable_apples:0,
+avaliable_milk:0,
+avaliable_carrot:0,
+value:'',
+
 }
 
-addItem=()=>{
-  console.log("dziala")
+addItem=(e)=>{
+  e.preventDefault();
+  let valueNumber= e.target.value;
+  // this.setState({
+  //  value: valueNumber,
+  // })
+
+  if(valueNumber==="0"){
+    if(this.state.avaliable_apples>=30){
+      alert("brak takiej ilości jabłek")
+    }else{ this.setState(prevState => ({
+      avaliable_apples: prevState.avaliable_apples + 1,
+    }))
+  }
+}
+  
+  else if(valueNumber==="1"){
+    if(this.state.avaliable_milk>=10){
+      alert("Brak wystarczającej ilości mleka")
+    } else {
+      this.setState( prevState => ({
+        avaliable_milk:prevState.avaliable_milk + 1,
+      }))
+    }
+    }
+     else if(valueNumber==="2"){
+       if(this.state.avaliable_carrot>=50){
+        alert("Brak wystarczającej ilości marchewki")
+       }else{
+        this.setState(prevState =>({
+          avaliable_carrot:prevState.avaliable_carrot + 1,
+        }))
+       }
+   
+  }
+  
 }
 
+
+removeItem=(e)=>{  
+  let valueNumber= e.target.value;
+  this.setState({
+   value: valueNumber,
+  })
+
+  if(this.state.value==="0"){
+    // if(this.state.avaliable_apples<=30){
+    //   this.setState({
+    //     avaliable_apples: this.state.avaliable_apples - 1,
+    //   })
+    // }else if (this.state.avaliable_apples===0){
+    //    alert("niewystarczająca liczba jablek")    
+    // console.log("jablka" + this.state.avaliable_apples)}
+    console.log("aaa")
+  }
+  
+  else if(this.state.value==="1"){
+    // if(this.state.avaliable_milk<=10){
+    //   this.setState({
+    //     avaliable_milk: this.state.avaliable_milk - 1,
+    //   })
+    //   console.log("mleko" + this.state.avaliable_milk)
+    //     } else  if (this.state.avaliable_milk===0) {
+     
+    //   console.log("niewystarczajaca ilosc mleka")
+    // }
+    console.log("b")
+  }
+     else if(this.state.value==="2"){
+      //  if(this.state.avaliable_carrot<=50){
+      //   this.setState({
+      //     avaliable_carrot: this.state.avaliable_carrot + 1,
+      //   })       }
+      //   else  if (this.state.avaliable_carrot===0){
+        
+      //   console.log("niewystarczajaca ilosc marchewki")
+      //  }
+   console.log("ce")
+  
+}
+}
 
   render(){
   return (
     <div className="App">
-      {this.state.item.map(({id, image, name, amount, cost})=>(
-            <Item key={image}
+      {this.state.item.map(({id, image, name, avaliableAmount, amount, cost})=>(
+        <Item key={id}
         id={id}
+        value={id}
         image={image}
         name={name}
+        avaliableAmount={avaliableAmount}
         amount={amount}
         cost={cost}
         submitFn={this.addItem}
+        submitFn2={this.removeItem}
         />
-
         ))}
-   
+  
+   {/* <p>suma  produktów w koszyku to {this.state.allAmount}</p>
+   <p>koszt to {this.state.sum}</p> */}
+     <p>jabłka: {this.state.avaliable_apples}</p> 
+     <p>mleko: {this.state.avaliable_milk}</p> 
+     <p>marchew: {this.state.avaliable_carrot}</p> 
     </div>
   );
 }
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const Item = ({image, id, name, amount, cost, submitFn}) =>(
+const Item = ({image, id, name, avaliableAmount, cost, submitFn, submitFn2, amount, value}) =>(
     <li>
         <div>Zdjęcie produktu:
             <img src={image} alt="a"/>
         </div>
        <p>Liczba porządkowa: {id}</p> 
        <p>{name}</p>
-       <p>Ilość dostępnych sztuk: {amount}</p>
+       <p>Ilość dostępnych sztuk: {avaliableAmount}</p>
+       <p>Ilość kupionych: {amount}</p>
        <p>Koszt produktu: {cost}</p>
 
-       <button onClick={submitFn}>Dodaj produkt</button>
-       <button onClick={submitFn}>Usuń produkt</button>
+        <button onClick={submitFn} value={value}>Dodaj produkt</button>
+        <button onClick={submitFn2} value={value}>Usuń produkt</button> 
+
 
     </li>
 )
