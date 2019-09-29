ff64465b1dee9b74ebb8141168065e786f30ac1e
Monika Fac <monika.fac@gmail.com>
2019-09-29 13:11:10 +0200

    added prices and sum

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     name:"jabłka",
     avaliableAmount:30,
     amount:0,
-    cost:2,
+    cost:0.58,
     
   },
   {
@@ -19,7 +19,7 @@ class App extends React.Component {
     name:"mleko",
     avaliableAmount:10,
     amount:0,
-    cost:3,
+    cost:1.3,
   },
   {
     id:2,
@@ -27,7 +27,7 @@ class App extends React.Component {
     name:"marchewka",
     avaliableAmount:50,
     amount:0,
-    cost:0.8,
+    cost:3.28,
   }
 ],
 
@@ -36,7 +36,11 @@ allAmount:0,
 avaliable_apples:0,
 avaliable_milk:0,
 avaliable_carrot:0,
-value:'',
+cost_of_apples:0.58,
+cost_of_milk:1.3,
+cost_of_carrot:3.28,
+
+
 
 }
 
@@ -44,6 +48,7 @@ addItem=(e)=>{
   e.preventDefault();
   let valueNumber= e.target.value;
   
+ // zmiana ilości 
 
   if(valueNumber==="0"){
     if(this.state.avaliable_apples>=30){
@@ -52,8 +57,7 @@ addItem=(e)=>{
       avaliable_apples: prevState.avaliable_apples + 1,
     }))
   }
-}
-  
+  }
   else if(valueNumber==="1"){
     if(this.state.avaliable_milk>=10){
       alert("Brak wystarczającej ilości mleka")
@@ -74,6 +78,7 @@ addItem=(e)=>{
    
   }
   
+
 }
 
 
@@ -134,9 +139,10 @@ removeItem=(e)=>{
   
    {/* <p>suma  produktów w koszyku to {this.state.allAmount}</p>
    <p>koszt to {this.state.sum}</p> */}
-     <p>jabłka: {this.state.avaliable_apples}</p> 
-     <p>mleko: {this.state.avaliable_milk}</p> 
-     <p>marchew: {this.state.avaliable_carrot}</p> 
+     <p>jabłka: {this.state.avaliable_apples} kg za 0.58 zł/kg, co daje {(this.state.avaliable_apples * this.state.cost_of_apples).toFixed(2)} </p> 
+     <p>mleko: {this.state.avaliable_milk} litra za 1.30 zł/litr, co daje {(this.state.avaliable_milk * this.state.cost_of_milk).toFixed(2)}</p> 
+     <p>marchew: {this.state.avaliable_carrot} kg za 3.28 zł/kg, co daje {(this.state.avaliable_carrot * this.state.cost_of_carrot).toFixed(2)} </p> 
+     <p>ogólny koszt: {(this.state.avaliable_apples * this.state.cost_of_apples + this.state.avaliable_milk * this.state.cost_of_milk + this.state.avaliable_carrot * this.state.cost_of_carrot).toFixed(2)} </p>
     </div>
   );
 }
